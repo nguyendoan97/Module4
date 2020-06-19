4a2e4b99142502cac5342e12d99905d1055526e3
nguyendoan97 <nguyenhongdoan14@gmail.com>
2020-06-19 16:07:25 +0700

    bai tap 4

---
+++ bai4/config_email/src/main/java/com/codegym/controller/EmailConfigController.java
@@ -0,0 +1,41 @@
+package com.codegym.controller;
+
+
+import com.codegym.model.EmailConfig;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class EmailConfigController {
+
+    @RequestMapping(value = "showform", method = RequestMethod.GET)
+    public String showForm(ModelMap model) {
+        model.addAttribute("emailConfig", new EmailConfig());
+        return "/create";
+    }
+
+    @ModelAttribute("languages")
+    public List<String> languages(){
+        List<String> list = new ArrayList<>();
+        list.add("English");
+        list.add("Vietnamese");
+        list.add("Thailand");
+        list.add("Chinese");
+        return list;
+    }
+    @RequestMapping(value = "addEmailConfig", method = RequestMethod.POST)
+    public String submit(@ModelAttribute("emailConfig") EmailConfig emailConfig, ModelMap model) {
+        model.addAttribute("language", emailConfig.getLanguage());
+        model.addAttribute("size", emailConfig.getSize());
+        model.addAttribute("spam", emailConfig.getSpam());
+        model.addAttribute("signature", emailConfig.getSignature());
+        return "/info";
+    }
+}
---
+++ bai4/config_email/src/main/java/com/codegym/model/EmailConfig.java
@@ -0,0 +1,40 @@
+package com.codegym.model;
+
+public class EmailConfig {
+    String language;
+    String size;
+    String spam;
+    String signature;
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getSpam() {
+        return spam;
+    }
+
+    public void setSpam(String spam) {
+        this.spam = spam;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+}

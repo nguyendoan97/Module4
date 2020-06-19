9c888c2093e2de6be07009b2c260d25a761542cf
nguyendoan97 <nguyenhongdoan14@gmail.com>
2020-06-19 17:02:31 +0700

    update email

--- bai4/config_email/src/main/java/com/codegym/controller/EmailConfigController.java
+++ bai4/config_email/src/main/java/com/codegym/controller/EmailConfigController.java
@@ -30,6 +30,18 @@ public class EmailConfigController {
         list.add("Chinese");
         return list;
     }
+    @ModelAttribute("sizes")
+    public List<String> sizes(){
+        List<String> list = new ArrayList<>();
+        list.add("0");
+        list.add("10");
+        list.add("15");
+        list.add("20");
+        list.add("25");
+        list.add("30");
+        list.add("35");
+        return list;
+    }
     @RequestMapping(value = "addEmailConfig", method = RequestMethod.POST)
     public String submit(@ModelAttribute("emailConfig") EmailConfig emailConfig, ModelMap model) {
         model.addAttribute("language", emailConfig.getLanguage());
